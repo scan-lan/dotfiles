836c4a2c82028fc075ae2d26d34aad3389c6b5b1
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-04-30 09:47:18 +0100

    fix(nvim): disable headlines and spectre plugins

--- home/dot_config/nvim/lua/plugins/disabled.lua
+++ home/dot_config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
 }
