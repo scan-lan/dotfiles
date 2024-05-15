1a305f5586c2fef6a4c67d6a8a3b82ee7e376558
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-05-15 13:43:25 +0100

    feat(kitty): ignore ctrl+l mapping when in nvim
    This was amended to remove the mapping in kitty for now, although it
    would be good to revisit.

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -24,6 +24,24 @@ end
 --   end,
 -- })
 
+-- These sourced from https://sw.kovidgoyal.net/kitty/mapping/#conditional-mappings-depending-on-the-state-of-the-focused-window
+vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
+  group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
+  callback = function()
+    io.stdout:write("\x1b]1337;SetUserVar=in_editor=MQo\007")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+  group = vim.api.nvim_create_augroup(
+    "KittyUnsetVarVimLeave",
+    { clear = true }
+  ),
+  callback = function()
+    io.stdout:write("\x1b]1337;SetUserVar=in_editor\007")
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
   group = augroup("wrap_words"),
   pattern = { "markdown", "typescriptreact", "javascriptreact", "html" },
