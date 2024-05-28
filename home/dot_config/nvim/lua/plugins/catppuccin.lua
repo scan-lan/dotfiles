e5b4b5dc61db1efac6396dcebd5e8d4289fa17f3
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-05-28 10:32:34 +0100

    fix(nvim): update catppuccin config

--- home/dot_config/nvim/lua/plugins/catppuccin.lua
+++ home/dot_config/nvim/lua/plugins/catppuccin.lua
@@ -4,7 +4,22 @@ return {
     name = "catppuccin",
     priority = 1000,
     opts = {
-      flavor = "frappe",
+      flavor = "auto",
+      background = {
+        light = "latte",
+        dark = "frappe",
+      },
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      integrations = {
+        mason = true,
+        noice = true,
+        lsp_trouble = true,
+        which_key = true,
+      },
     },
   },
 
