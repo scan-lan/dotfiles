ecc9ef592961f77d2f8e1e6cd1c11ae31299a9c6
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-07-08 15:45:25 +0100

    refactor(skhd): move back/fwd commands to scripts

---
+++ home/dot_config/scripts/executable_bck_window.sh
@@ -0,0 +1,5 @@
+yabai -m query --spaces --space |
+	jq -re '.index' |
+	xargs -I{} yabai -m query --windows --space {} |
+	jq -sre 'add | map(select(.["is-minimized"] == false)) | map(select((.app | test(".*Microsoft Team.*")) and .["is-floating"] == true | not)) | sort_by(.display, .frame.y, .frame.x, .id) | nth(index(map(select(.["has-focus"] == true))) - 1).id' |
+	xargs -I{} yabai -m window --focus {}
