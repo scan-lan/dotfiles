4fbcba3d2d7f0e8ed61b0392de4e178b97dd7866
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-07-08 10:14:52 +0100

    fix(nvim): update headlines config

--- home/dot_config/nvim/lua/plugins/disabled.lua
+++ home/dot_config/nvim/lua/plugins/disabled.lua
@@ -2,5 +2,4 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
-  { "lukas-reineke/headlines.nvim", enabled = false },
 }
