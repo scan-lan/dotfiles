da50ca89cb8aa1da7f087862305888ca6e50d575
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-07-16 16:52:36 +0100

    feat(nvim): configure flash.nvim for canary

---
+++ home/dot_config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    labels = "tnsericahdfpyolugmvjqwbzkx",
+    jump = {
+      autojump = true,
+      -- clear highlight after jump
+      nohlsearch = true,
+      -- Enable this to use rainbow colors to highlight labels
+      -- Can be useful for visualizing Treesitter ranges.
+      rainbow = {
+        enabled = true,
+        -- number between 1 and 9
+        shade = 5,
+      },
+    },
+  },
+}
