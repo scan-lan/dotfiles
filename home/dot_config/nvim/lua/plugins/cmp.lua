300a4cbda531514c022d913199127c5c94e788e7
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-08-16 11:58:19 +0100

    feat(nvim): add <C-Space> mapping to trigger completion

--- home/dot_config/nvim/lua/plugins/cmp.lua
+++ home/dot_config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,11 @@
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    enabled = false,
-  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-Space>"] = cmp.mapping.complete(),
+    })
+  end,
 }
