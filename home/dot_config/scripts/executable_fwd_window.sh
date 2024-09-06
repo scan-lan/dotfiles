e1d82fc2ca312c885484abf84debddfa37ae0a99
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-09-06 10:17:10 +0100

    feat(skhd): shortcuts no longer select windows that don't have layer "normal"
    Hopefully this works!

--- home/dot_config/scripts/executable_fwd_window.sh
+++ home/dot_config/scripts/executable_fwd_window.sh
@@ -1,5 +1,10 @@
 yabai -m query --spaces --space |
 	jq -re '.index' |
 	xargs -I{} yabai -m query --windows --space {} |
-	jq -sre 'add | map(select(.["is-minimized"] == false)) | map(select((.app | test(".*Microsoft Team.*")) and .["is-floating"] == true | not)) | sort_by(.display, .frame.y, .frame.x, .id) | reverse | nth(index(map(select(.["has-focus"] == true))) - 1).id' |
+	jq -sre 'add \
+| map(select(.["is-minimized"] == false)) \
+| map(select(.layer == "normal")) \
+| sort_by(.display, .frame.y, .frame.x, .id) \
+| reverse \
+| nth(index(map(select(.["has-focus"] == true))) - 1).id' |
 	xargs -I{} yabai -m window --focus {}
