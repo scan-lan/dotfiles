cd72df37cc0857802e8f9d7993cb5880196d6227
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-09-16 12:05:28 +0100

    feat(nvim): update obsidian.nvim to handle daily notes correctly

--- home/dot_config/nvim/lua/plugins/obsidian.lua
+++ home/dot_config/nvim/lua/plugins/obsidian.lua
@@ -39,6 +39,11 @@ return {
         -- vim.fn.jobstart({"xdg-open", url})  -- linux
       end,
 
+      -- Where to put new notes. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "current_dir",
+
       workspaces = {
         {
           name = "brain",
@@ -61,19 +66,47 @@ return {
       --   end
       --   return false
       -- end,
+
+      daily_notes = {
+        -- Optional, if you keep daily notes in a separate directory.
+        folder = "dailies",
+        -- Optional, if you want to change the date format for the ID of daily notes.
+        -- date_format = "%Y-%m-%d",
+        -- Optional, if you want to change the date format of the default alias of daily notes.
+        alias_format = "%a %-d %B, %Y",
+        -- Optional, default tags to add to each new daily note created.
+        default_tags = { "dailies" },
+        -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+        template = "today.md",
+      },
+
+      -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
+      -- way then set 'mappings = {}'.
+      mappings = {
+        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        -- Toggle check-boxes.
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        -- Smart action depending on context, either follow link or toggle checkbox.
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
     },
-    daily_notes = {
-      -- Optional, if you keep daily notes in a separate directory.
-      folder = "dailies",
-      -- Optional, if you want to change the date format for the ID of daily notes.
-      -- date_format = "%Y-%m-%d",
-      -- Optional, if you want to change the date format of the default alias of daily notes.
-      -- alias_format = "%B %-d, %Y",
-      -- Optional, default tags to add to each new daily note created.
-      -- default_tags = { "daily-notes" },
-      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-      template = nil,
-    },
+
     keys = {
       { "<leader>ob", "<Cmd>ObsidianBacklinks<CR>", desc = "Show backlinks" },
       { "<leader>on", "<Cmd>ObsidianNew<CR>", desc = "New note" },
