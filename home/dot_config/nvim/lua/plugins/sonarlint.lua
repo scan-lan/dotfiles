361c1f083ed6b47927a9370e4dd9b79412ca7a4c
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2024-10-11 14:51:14 +0100

    feat(nvim): add sonarlint

---
+++ home/dot_config/nvim/lua/plugins/sonarlint.lua
@@ -0,0 +1,29 @@
+return {
+  "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  opts = {
+    server = {
+      cmd = {
+        "sonarlint-language-server",
+        -- Ensure that sonarlint-language-server uses stdio channel
+        "-stdio",
+        "-analyzers",
+        -- paths to the analyzers you need, using those for python and java in this example
+        -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+        -- vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjs.jar"),
+        vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+      },
+    },
+    filetypes = {
+      -- Tested and working
+      -- "python",
+      -- "cpp",
+      "typescript",
+      "typescriptreact",
+      "javascript",
+      "javascriptreact",
+      "js",
+      "java",
+    },
+  },
+}
