fe4f67830a6f88d2a9cd9716f5938d88783fbf8b
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2025-06-30 10:53:19 +0100

    feat(fish): add codeartifact auth function

---
+++ home/dot_config/fish/functions/auth-codeartifact.fish
@@ -0,0 +1,5 @@
+function auth-codeartifact
+    set -x CODEARTIFACT_AUTHORIZATION_TOKEN "$(aws codeartifact get-authorization-token --domain plg-tech --domain-owner 905418218692 --region eu-central-1 --query authorizationToken --output text)"
+    set -x POETRY_HTTP_BASIC_PLG_USERNAME aws
+    set -gx POETRY_HTTP_BASIC_PLG_PASSWORD "$CODEARTIFACT_AUTHORIZATION_TOKEN"
+end
