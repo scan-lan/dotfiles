646ecf3876314819cbe8e6ae60710a6acdb8d6d1
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2025-07-15 10:28:15 +0100

    feat(nvim): add plugin telescope live grep args

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,12 @@ return {
   "nvim-telescope/telescope.nvim",
   dependencies = {
     "nvim-telescope/telescope-file-browser.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    },
   },
   keys = {
     {
