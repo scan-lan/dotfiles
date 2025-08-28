95539c2c97b05ff1678304e645f1c9d51a2dd099
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2025-08-28 10:59:34 +0100

    fix(nvim): update bufferline for catpuccin breaking change

--- home/dot_config/nvim/lua/plugins/bufferline.lua
+++ home/dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,11 @@
 return {
   "akinsho/bufferline.nvim",
+  init = function()
+    local bufline = require("catppuccin.groups.integrations.bufferline")
+    function bufline.get()
+      return bufline.get_theme()
+    end
+  end,
   opts = {
     options = {
       always_show_bufferline = true,
